4c1f32cc464fb13a9f6ee0175dceed4f0711a2de
Muthu011 <muthuvijayakumar5@gmail.com>
2023-01-29 11:24:31 +0530

    Update generate-gifts.js

--- pages/api/generate-gifts.js
+++ pages/api/generate-gifts.js
@@ -1,7 +1,7 @@
 import { Configuration, OpenAIApi } from 'openai';
 
 const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+  apiKey: "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",
 });
 const openai = new OpenAIApi(configuration);
 
